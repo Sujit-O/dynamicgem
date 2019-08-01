68c1dc6099bf70da6aa554d58352c37d8d55187b
Sujit-O <sujitchhetri@gmail.com>
2019-08-01 18:24:10 -0400

    adding TRIP

--- dynamicgem/utils/timers_utils.py
+++ dynamicgem/utils/timers_utils.py
@@ -53,7 +53,7 @@ def parseData(filepath, M):
 					col.append(toNode)
 					data.append(1)
 
-	return csr_matrix((data, (row, col)), shape=(len(M), len(M)))
+	return csc_matrix((data, (row, col)), shape=(len(M), len(M)))
 
 def Obj(Sim, U, V):
 	"""Function to  calculate the objective funciton or loss.
@@ -95,6 +95,59 @@ def Obj(Sim, U, V):
 
     # return L
 
+def deltaA(A, filepath,M):
+	"""Function to  get the difference between previous and current graph.
+           
+        Args:
+            A (Sparse Matrix): Sparse scipy adjancey matrix
+            M (dict): Dictionary of aribitrary nodes to consecutive node values
+            filepath (str): Txt path where the graph is stored.
+	
+        Returns:
+        	Sparse Matrix: The delta graph
+    """
+    A_new = parseData(filepath,M)
+    L = A.shape[0]
+    S_delta = csc_matrix(([0], ([0], [0])), shape=(L,L))
+
+    [i_old, j_old, val_old] = find(A)
+	[i_new, j_new, val_new] = find(A_new)
+
+	for k in range(len(i_new)):
+		temp_old = A[i_new[k],j_new[k]]
+		S_delta[i_new(k),j_new(k)]=val_new[k]-temp_old
+
+	for k in range(len(i_old)):
+		temp_new = A_new[i_old[k],j_old[k]]
+		S_delta[i_old[k],j_old[k]]=temp_new-val_old[k]
+
+	return S_delta
+
+def TRIP(Old_U,Old_S,Old_V, Delta_A):
+	"""Function to  calculate the new embedding by utilizing the differene graph.
+
+	 	Update the embedding using TRIP method reference: Chen Chen, and Hanghang Tong. 
+		"Fast eigen-functions tracking on dynamic graphs." SDM, 2015.
+           
+        Args:
+            Old_U (Matrix): old N x K left embedding vector
+            Old_S (Matrix): old K x K  embedding vector
+            Old_V (Matrix): old N x K right embedding vector
+            DeltaA (Sparse Matrix): The difference of the two graphs.
+	
+        Returns:
+        	 Matrices: New_U, New_S, New_V
+    """
+    N, K = np.shape(Old_U)
+    Old_X = Old_U
+    for i in range(K):
+    	temp_i = np.argmax(np.absolute(Old_X[:,i]))
+    	if Old_X[temp_i,i]<0:
+    		Old_X[:,i]= - Old_X[:,i]
+    temp_v = Old_U.max()
+    temp_i = np.argmax(Old_U)
+
+    
 
 
 
@@ -132,9 +185,10 @@ def TIMERS(dataFolder, K, Theta, datatype):
   	A = parseData(dataFolder+'/0', M)
   	# Initialize the symmetric matrix
   	Sim = A
+  	N = A.shape[0]
 
   	# calculate static solution
-  	U[0], S[0], Vh[0] = svds(A, K)
+  	U[0], S[0], V[0] = svds(A, K)
 
   	U_cur = U[0] * np.sqrt(S[0])
   	V_cur - S[0] * np.sqrt(S[0])
@@ -155,6 +209,25 @@ def TIMERS(dataFolder, K, Theta, datatype):
 	Loss_store[0] = Obj(Sim, U_cur, V_cur)
 	# assign the bound as current loss
 	Loss_bound[0] = Loss_store[0]
+
+	# Store the cummulative similairty matrix
+	S_cum = Sim
+	# Store the cumulative perturbation from last rerun
+	S_perturb = csc_matrix(([0], ([0], [0])), shape=(N, N))
+
+	loss_rerun = Loss_store[0]
+
+	for i in range(1,time_slice):
+		S_add = deltaA(S_cum,dataFolder+'/'+str(i),M)
+		S_perturb = S_perturb + S_add
+
+		if (Update):
+			U[i], S[i], V[i] = TRIP(U[i-1],S[i-1],V[i-1],S_add)
+			# Note: TRIP does not insure smaller value
+
+
+
+
 
 
 
